27fc58feb58fd3fe41488bffe72706d3bde58b49
Big_Carp <tobilobad1738@gmail.com>
2026-02-28 21:50:59 +0100

    cleanup

--- components/GoogleButton.tsx
+++ components/GoogleButton.tsx
@@ -7,7 +7,7 @@ export default function GoogleButton() {
         await supabase.auth.signInWithOAuth({
             provider: "google",
             options: {
-                redirectTo: `${window.location.origin}/auth/callback`
+                redirectTo: `${window.location.origin}/home`
             }
         })
     }

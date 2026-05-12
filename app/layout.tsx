962f5de36e65b6916b4ad1179027bf74c67b3bbe
Big_Carp <tobilobad1738@gmail.com>
2026-05-12 10:10:31 +0100

    favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,6 +58,7 @@ export default function RootLayout({
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#1e293b" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="icon" href="/favicon.ico" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
       </head>
